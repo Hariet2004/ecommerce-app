f3ae453a344c5a05692894d21fc4a30306bf98cb
Hariet2004 <hariet@leja.co.ke>
2024-06-24 15:16:10 +0300

    added some sruff

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Header() {
   return (
+    <>
+    
     <div className="header">
       {/* Image logo */}
       <div className="header__logo">
@@ -17,28 +19,34 @@ function Header() {
       
       {/* Search bar */}
       <div className="header__search">
-        <input type="text" placeholder="Search items or products" />
+        <input className="header__input" type="text" placeholder="Search items or products" />
         <SearchOutlinedIcon className="header__searchIcon" />
       </div>
       
       {/* Navigation options */}
       <div className="header__nav">
         <div className="header__option">
-          <h4>Hello Wambui</h4>
-          <h4>Accounts & Lists</h4>
+          <h4 className="headerText">Hello Wambui</h4>
+          <h4 className="headerText">Accounts & Lists</h4>
         </div>
         
         <div className="header__option">
-          <h4>Return</h4>
-          <h4>& Orders</h4>
+          <h4 className="headerText">Return</h4>
+          <h4 className="headerText">& Orders</h4>
         </div>
         
         <div className="header__optionCart">
           <ShoppingCartIcon className="header__cartIcon" />
           <span className="header__cartCount">0</span>
         </div>
+        
+        <div className="header__option">
+          <h4 className="headerText">Kenya</h4>
+          <h4 className="headerText">0798884016</h4>
+        </div>
       </div>
     </div>
+    </>
   );
 }
 
